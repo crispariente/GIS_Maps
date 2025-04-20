79944a9e8968ce39543ee54a9d773a96e8e1a191
crispariente <cristianabraham3@hotmail.com>
2025-04-20 11:33:28 -0600

    new map who dis

--- styles/Isochrones_2_style.js
+++ styles/Isochrones_2_style.js
@@ -1,14 +1,14 @@
 var size = 0;
 var placement = 'point';
 
-var style_CityLimitsCountyBoundary_3 = function(feature, resolution){
+var style_Isochrones_2 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
     };
     
     var labelText = ""; 
-    var value = feature.get("''");
+    var value = feature.get("");
     var labelFont = "10px, sans-serif";
     var labelFill = "#000000";
     var bufferColor = "";
@@ -20,11 +20,12 @@ var style_CityLimitsCountyBoundary_3 = function(feature, resolution){
     if ("" !== null) {
         labelText = String("");
     }
-    
     var style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(45,145,45,0.5)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor, bufferWidth)
-    })];;
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
 
     return style;
 };
